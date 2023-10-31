53faeea3e64d31b243c366b517811b70788edee8
CypherKE <59822938+kamden02@users.noreply.github.com>
2023-10-31 21:50:22 +0300

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+
+class AddLocationForm(BoxLayout):
+	search_input = ObjectProperty()
+
+	def search_location(self):
+		print("The user searched for '{}'".format(self.search_input.text))
+
+class trial(App):
+	pass
+
+if __name__ == '__main__':
+	trial().run()
